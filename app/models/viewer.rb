e8ff1a805f49b11d2fec822e96920106215a2d8b
skarolk <skarolkiewicz@gmail.com>
2018-09-14 10:21:28 -0400

    CRUSHED IT

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,36 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select do |item|
+      item.viewer == self
+    end
+  end
+
+  def queue_movies
+    self.queue_items.map do |item|
+      item.movie
+    end
+  end
+
+  def add_movie_to_queue(movie)
+    new_movie = QueueItem.new(self, movie)
+  end
+
+  def rate_movie(movie, rating)
+    queue = self.queue_items
+    target_item = queue.find do |item|
+      item.movie == movie
+    end
+    target_item.rating = 5
+    target_item
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,24 @@ def reload
   load 'config/environment.rb'
 end
 
+m1 = Movie.new("Jurassic Park")
+m2 = Movie.new("Lord of the Rings")
+m3 = Movie.new("Harry Potter")
+
+v1 = Viewer.new("bobross")
+v2 = Viewer.new("tattooguy")
+v3 = Viewer.new("bestsabrina")
+
+qi1 = QueueItem.new(v1, m2, 5)
+qi2 = QueueItem.new(v2, m1, 4)
+qi3 = QueueItem.new(v2, m3, 4)
+qi4 = QueueItem.new(v3, m3, 3)
+
+# v3.add_movie_to_queue(m1)
+# v3.add_movie_to_queue(m2)
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
